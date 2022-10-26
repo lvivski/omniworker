d205a3a81dd763e25759671fe852744ccfe761d5
Yehor Lvivski <yehor@lvivski.com>
2022-10-26 15:19:15 +0200

    SharedWorker via data url

--- index.ts
+++ index.ts
@@ -1,24 +1,29 @@
 interface OmniWorkerOptions extends WorkerOptions {
-	shared?: boolean;
+	shared?: boolean
 }
 
 interface OmniWorkerInterface {
-	new <T extends OmniWorkerOptions>(scriptUrl: string | URL, options?: T): T["shared"] extends true
+	new <T extends OmniWorkerOptions>(scriptUrl: string | URL, options?: T): T['shared'] extends true
 		? SharedWorker
-		: Worker;
+		: Worker
 }
 
 class OmniWorker {
 	constructor(scriptUrl: string | URL, options?: OmniWorkerOptions) {
-		const name = scriptUrl.toString().split("/").pop();
-		const objectUrl = URL.createObjectURL(
-			new Blob([`importScripts(${JSON.stringify(scriptUrl)});`], { type: "application/javascript" })
-		);
-		const WorkerClass = options?.shared ? SharedWorker : Worker;
-		const worker = new WorkerClass(objectUrl, { name, ...options });
-		URL.revokeObjectURL(objectUrl);
-		return worker;
+		const name = scriptUrl.toString().split('/').pop()
+		const workerTemplate = `importScripts(${JSON.stringify(scriptUrl)});`
+		const type = 'application/javascript'
+
+		let worker: Worker | SharedWorker
+		if (options?.shared) {
+			worker = new SharedWorker(`data:${type};base64,` + btoa(workerTemplate), { name, ...options })
+		} else {
+			const objectUrl = URL.createObjectURL(new Blob([workerTemplate], { type }))
+			worker = new Worker(objectUrl, { name, ...options })
+			URL.revokeObjectURL(objectUrl)
+		}
+		return worker
 	}
 }
 
-export default OmniWorker as OmniWorkerInterface;
+export default OmniWorker as OmniWorkerInterface
